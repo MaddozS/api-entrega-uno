1f5c49985c4a6c47ab1670c3a049408303d323e7
Axel Anaya <axel.ana25@gmail.com>
2022-04-23 15:13:34 -0500

    fix http code response

--- api/alumnos.py
+++ api/alumnos.py
@@ -27,7 +27,7 @@ def get_alumno(id_alumno: int, response: Response):
     response.status_code = 404
     return {'message': 'Alumno no encontrado'}
 
-@router.put("/{id_alumno}", status_code=201)
+@router.put("/{id_alumno}", status_code=200)
 def put_alumno(id_alumno: int, alumno: Alumno):
   alumnos[id_alumno] = alumno
   return alumno
--- api/profesores.py
+++ api/profesores.py
@@ -27,7 +27,7 @@ def get_profesor(id_profesor: int, response: Response):
     response.status_code = 404
     return {'message': 'Profesor no encontrado'}
 
-@router.put("/{id_profesor}", status_code=201)
+@router.put("/{id_profesor}", status_code=200)
 def put_profesor(id_profesor: int, profesor: Profesor):
   profesores[id_profesor] = profesor
   return profesor
