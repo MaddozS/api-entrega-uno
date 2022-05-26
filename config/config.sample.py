280f5ecb2a6ebf70a67929ad5095d6413de6a8c0
Axel Anaya <axel.ana25@gmail.com>
2022-05-25 19:42:22 -0500

    Added config package

---
+++ config/config.sample.py
@@ -0,0 +1,17 @@
+# AWS Config
+
+config = {
+  "AWS_REGION_NAME": '',
+  "AWS_ACCESS_KEY_ID": '',
+  "AWS_SECRET_ACCESS_KEY": '',
+  "AWS_SESSION_TOKEN": "",
+  "AWS_S3_BUCKET": '',
+
+  # DB Config
+  "DATABASE_MANAGER": 'postgresql',
+  "DATABASE_HOST": '',
+  "DATABASE_PORT": '5432',
+  "DATABASE_NAME": '',
+  "DATABASE_USER": '',
+  "DATABASE_PASSWORD": ''
+}
