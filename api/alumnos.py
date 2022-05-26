159d6fdbd007d45a79224d27779ac3ded19e6abc
Axel Anaya <axel.ana25@gmail.com>
2022-05-25 19:43:40 -0500

    API now uses the db crud

--- api/alumnos.py
+++ api/alumnos.py
@@ -1,42 +1,78 @@
 
-from fastapi import APIRouter, Response
+import os
+from fastapi import APIRouter, Depends, File, HTTPException, UploadFile
 
-from models.Alumno import Alumno
+from typing import List
+
+from sqlalchemy.orm import Session
+from aws.client import S3Client
+
+from db_app.database import engine, Base, get_db
+from db_app.schemas.Alumno import AlumnoCreate, Alumno
+from db_app.crud import alumnos
+
+Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
 
-index = 1 
-alumnos = {}
-
-@router.get("", status_code=200)
-def get_alumnos():
-  return alumnos
+@router.get("", status_code=200, response_model=List[Alumno])
+def get_alumnos(db: Session = Depends(get_db)):
+  return alumnos.get_alumnos(db)
 
 @router.post("", status_code=201, )
-def post_alumno(alumno: Alumno):
-  alumnos[alumno.id] = alumno
-  return alumno
+def post_alumno(alumno: AlumnoCreate, db: Session = Depends(get_db)):
+  return alumnos.create_alumno(db, alumno)
 
 @router.get("/{id_alumno}", status_code=200)
-def get_alumno(id_alumno: int, response: Response):
+def get_alumno(id_alumno: int, db: Session = Depends(get_db)):
 
-  if id_alumno in alumnos:
-    alumno = alumnos[id_alumno]
-    return alumno
-  else:
-    response.status_code = 404
-    return {'message': 'Alumno no encontrado'}
+  alumno_db = alumnos.get_alumno(db, id_alumno)
+
+  if not alumno_db:
+    raise HTTPException(status_code=404, detail="Alumno no encontrado")
+  
+  return alumno_db
 
 @router.put("/{id_alumno}", status_code=200)
-def put_alumno(id_alumno: int, alumno: Alumno):
-  alumnos[id_alumno] = alumno
-  return alumno
+def put_alumno(id_alumno: int, alumno: AlumnoCreate, db: Session = Depends(get_db)):
+  
+  alumno_db = alumnos.get_alumno(db, id_alumno)
+
+  if not alumno_db:
+    raise HTTPException(status_code=404, detail="Alumno no encontrado")
+  
+  return alumnos.edit_alumno(db, id_alumno, alumno)
 
 @router.delete("/{id_alumno}")
-def delete_alumno(id_alumno: int, response: Response):
-  if id_alumno in alumnos:
-    del alumnos[id_alumno]
-    return {'message': 'Alumno eliminado'}
-  else:
-    response.status_code = 404
-    return {'message': 'Alumno no encontrado'}
+def delete_alumno(id_alumno: int, db: Session = Depends(get_db)):
+    
+    alumno_db = alumnos.get_alumno(db, id_alumno)
+  
+    if not alumno_db:
+      raise HTTPException(status_code=404, detail="Alumno no encontrado")
+    
+    return alumnos.delete_alumno(db, id_alumno)
+
+@router.post("/{id_alumno}/fotoPerfil")
+def add_foto_perfil_alumno(id_alumno: int, foto: UploadFile = File(...), db: Session = Depends(get_db)):
+    alumno_db = alumnos.get_alumno(db, id_alumno)
+
+    if not alumno_db:
+      raise HTTPException(status_code=404, detail="Alumno no encontrado")
+
+    split_file_name = os.path.splitext(foto.filename)  #split the file name into two different path (string + extention)
+    file_name_unique = alumno_db.matricula
+    file_extension = split_file_name[1]
+    
+    data = foto.file._file
+    s3 = S3Client()
+    url = s3.upload_file(data, f"alumnos/{file_name_unique}{file_extension}")
+
+    alumno_foto = AlumnoCreate(
+      nombres = alumno_db.nombres,
+      apellidos = alumno_db.apellidos,
+      matricula = alumno_db.matricula,
+      promedio=alumno_db.promedio,
+      fotoPerfilUrl=url)
+
+    return alumnos.edit_alumno(db, id_alumno, alumno_foto)
--- api/profesores.py
+++ api/profesores.py
@@ -1,42 +1,51 @@
+from typing import List
 
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Depends, HTTPException
 
-from models.Profesor import Profesor
+from sqlalchemy.orm import Session
+
+from db_app.database import engine, Base, get_db
+from db_app.schemas.Profesor import ProfesorCreate, Profesor
+from db_app.crud import profesores
+
+Base.metadata.create_all(bind=engine)
 
 router = APIRouter()
 
-index = 1 
-profesores = {}
+@router.get("", status_code=200, response_model=List[Profesor])
+def get_profesores(db: Session = Depends(get_db)):
+  return profesores.get_profesores(db)
 
-@router.get("", status_code=200)
-def get_profesores():
-  return profesores
-
-@router.post("", status_code=201)
-def post_profesor(profesor: Profesor):
-  profesores[profesor.id] = profesor
-  return profesor
+@router.post("", status_code=201 )
+def post_alumno(alumno: ProfesorCreate, db: Session = Depends(get_db)):
+  return profesores.create_profesor(db, alumno)
 
 @router.get("/{id_profesor}", status_code=200)
-def get_profesor(id_profesor: int, response: Response):
+def get_profesor(id_profesor: int, db: Session = Depends(get_db)):
 
-  if id_profesor in profesores:
-    profesor = profesores[id_profesor]
-    return profesor
-  else:
-    response.status_code = 404
-    return {'message': 'Profesor no encontrado'}
+  profesor_db = profesores.get_profesor(db, id_profesor)
+
+  if not profesor_db:
+    raise HTTPException(status_code=404, detail="Profesor no encontrado")
+  
+  return profesor_db
 
 @router.put("/{id_profesor}", status_code=200)
-def put_profesor(id_profesor: int, profesor: Profesor):
-  profesores[id_profesor] = profesor
-  return profesor
+def put_profesor(id_profesor: int, profesor: ProfesorCreate, db: Session = Depends(get_db)):
+  
+  profesor_db = profesores.get_profesor(db, id_profesor)
+
+  if not profesor_db:
+    raise HTTPException(status_code=404, detail="Profesor no encontrado")
+  
+  return profesores.edit_profesor(db, id_profesor, profesor)
 
 @router.delete("/{id_profesor}")
-def delete_profesor(id_profesor: int, response: Response):
-  if id_profesor in profesores:
-    del profesores[id_profesor]
-    return {'message': 'Profesor eliminado'}
-  else:
-    response.status_code = 404
-    return {'message': 'Profesor no encontrado'}
+def delete_profesor(id_profesor: int, db: Session = Depends(get_db)):
+    
+    profesor_db = profesores.get_profesor(db, id_profesor)
+  
+    if not profesor_db:
+      raise HTTPException(status_code=404, detail="Alumno no encontrado")
+    
+    return profesores.delete_profesor(db, id_profesor)
